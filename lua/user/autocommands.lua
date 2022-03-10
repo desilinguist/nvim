4340537258dae2b7cb53ce51cbfb905ceb830d59
Nitin Madnani <nmadnani@ets.org>
2022-03-10 15:57:32 -0500

    Add autocommands for python and rst.

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -2,8 +2,8 @@
 vim.cmd([[
   augroup _general_settings
     autocmd!
-    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR> 
-    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Search', timeout = 200}) 
+    autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
+    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Search', timeout = 200})
     autocmd BufWinEnter * :set formatoptions-=cro
     autocmd FileType qf set nobuflisted
   augroup end
@@ -14,15 +14,22 @@ vim.cmd([[
     autocmd FileType gitcommit setlocal spell
   augroup end
 
-  augroup _markdown
+  augroup _markdown_and_rst
     autocmd!
-    autocmd FileType markdown setlocal wrap
-    autocmd FileType markdown setlocal spell
+    autocmd FileType markdown,rst setlocal wrap
+    autocmd FileType markdown,rst setlocal spell
+    autocmd FileType markdown,rst setlocal colorcolumn=80
+    autocmd FileType markdown,rst setlocal textwidth=79
+  augroup end
+
+  augroup _python
+    autocmd!
+    autocmd FileType python setlocal colorcolumn=80,100
   augroup end
 
   augroup _auto_resize
     autocmd!
-    autocmd VimResized * tabdo wincmd = 
+    autocmd VimResized * tabdo wincmd =
   augroup end
 
   augroup _alpha

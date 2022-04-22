18af8f9442aacbca30468f9a66d7be7526a7a046
Nitin Madnani <nmadnani@ets.org>
2022-04-21 21:35:11 -0400

    Add plugin to make j/k movements faster.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -156,6 +156,22 @@ return packer.startup(function(use)
 	-- plugin to show current scope in status line
 	use("SmiteshP/nvim-gps")
 
+	-- plugin to make j/k movement faster
+	use({
+		"PHSix/faster.nvim",
+		event = { "VimEnter *" },
+		config = function()
+			-- vim.api.nvim_set_keymap("n", "j", "<Plug>(faster_move_j)", { noremap = false, silent = true })
+			-- vim.api.nvim_set_keymap("n", "k", "<Plug>(faster_move_k)", { noremap = false, silent = true })
+			-- or
+			vim.api.nvim_set_keymap('n', 'j', '<Plug>(faster_move_gj)', {noremap=false, silent=true})
+			vim.api.nvim_set_keymap('n', 'k', '<Plug>(faster_move_gk)', {noremap=false, silent=true})
+			-- if you need map in visual mode
+			-- vim.api.nvim_set_keymap('v', 'j', '<Plug>(faster_vmove_j)', {noremap=false, silent=true})
+			-- vim.api.nvim_set_keymap('v', 'k', '<Plug>(faster_vmove_k)', {noremap=false, silent=true})
+		end,
+	})
+
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
 	if PACKER_BOOTSTRAP then

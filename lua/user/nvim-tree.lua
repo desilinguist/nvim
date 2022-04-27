0fe64ea93ca0ac68ade987a48a9813fcad733291
Nitin Madnani <nmadnani@ets.org>
2022-04-27 18:30:23 -0400

    Update nvim-tree.lua with latest options.

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -47,7 +47,7 @@ nvim_tree.setup({
 	open_on_tab = false,
 	hijack_cursor = true,
 	update_cwd = true,
-	update_to_buf_dir = {
+	hijack_directories = {
 		enable = true,
 		auto_open = true,
 	},
@@ -58,6 +58,26 @@ nvim_tree.setup({
 			info = "",
 			warning = "",
 			error = "",
+		},
+	},
+	actions = {
+		use_system_clipboard = true,
+		change_dir = {
+			enable = true,
+			global = false,
+			restrict_above_cwd = false,
+		},
+		open_file = {
+			quit_on_open = false,
+			resize_window = true,
+			window_picker = {
+				enable = true,
+				chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+				exclude = {
+					filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+					buftype = { "nofile", "terminal", "help" },
+				},
+			},
 		},
 	},
 	update_focused_file = {
@@ -83,7 +103,6 @@ nvim_tree.setup({
 		height = 30,
 		hide_root_folder = false,
 		side = "left",
-		auto_resize = true,
 		mappings = {
 			custom_only = false,
 			list = {
@@ -98,16 +117,5 @@ nvim_tree.setup({
 	trash = {
 		cmd = "trash",
 		require_confirm = true,
-	},
-	quit_on_open = 0,
-	git_hl = 1,
-	disable_window_picker = 0,
-	root_folder_modifier = ":t",
-	show_icons = {
-		git = 1,
-		folders = 1,
-		files = 1,
-		folder_arrows = 1,
-		tree_width = 40,
 	},
 })

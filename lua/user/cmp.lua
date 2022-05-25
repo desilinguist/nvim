db37e475e30b5f826fa5aba3fb8625c7d2a081c6
Nitin Madnani <nmadnani@ets.org>
2022-05-24 20:53:40 -0400

    Use default borders for cmp completion and docs.

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -122,9 +122,8 @@ cmp.setup({
 		select = false,
 	},
 	window = {
-		documentation = {
-			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-		},
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
 	},
 	experimental = {
 		ghost_text = false,

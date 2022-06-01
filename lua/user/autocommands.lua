b470c2d8300da00f44bc43f2b708fa3753632b00
Nitin Madnani <nmadnani@ets.org>
2022-06-01 09:19:15 -0400

    Reformat autocommands.lua

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,63 +1,69 @@
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir" },
-  callback = function()
-    vim.cmd [[
+	pattern = { "qf", "help", "man", "lspinfo", "spectre_panel", "lir" },
+	callback = function()
+		vim.cmd([[
       nnoremap <silent> <buffer> q :close<CR>
       set nobuflisted
-    ]]
-  end,
+    ]])
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-  callback = function()
-    vim.cmd "set formatoptions-=cro"
-  end,
+	callback = function()
+		vim.cmd("set formatoptions-=cro")
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-  callback = function()
-    vim.highlight.on_yank { higroup = "Search", timeout = 200 }
-  end,
+	callback = function()
+		vim.highlight.on_yank({ higroup = "Search", timeout = 200 })
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "VimResized" }, {
-  callback = function()
-    vim.cmd "tabdo wincmd ="
-  end,
+	callback = function()
+		vim.cmd("tabdo wincmd =")
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "gitcommit" },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-  end,
+	pattern = { "gitcommit" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "markdown", "rst" },
-  callback = function()
-    vim.opt_local.wrap = true
-    vim.opt_local.spell = true
-    vim.opt_local.textwidth = 79
-    vim.opt_local.colorcolumn = "+1"
-  end,
+	pattern = { "tf" },
+	callback = function()
+		vim.cmd("set filetype=terraform")
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = { "python" },
-  callback = function()
-    vim.opt_local.textwidth = 120
-    vim.opt_local.colorcolumn = "-40,-20"
-  end,
+	pattern = { "markdown", "rst" },
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+		vim.opt_local.textwidth = 79
+		vim.opt_local.colorcolumn = "+1"
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "python" },
+	callback = function()
+		vim.opt_local.textwidth = 120
+		vim.opt_local.colorcolumn = "-40,-20"
+	end,
 })
 
 vim.api.nvim_create_autocmd({ "User" }, {
-  pattern = { "AlphaReady" },
-  callback = function()
-    vim.cmd [[
+	pattern = { "AlphaReady" },
+	callback = function()
+		vim.cmd([[
       set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
-    ]]
-  end,
+    ]])
+	end,
 })
-

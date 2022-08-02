d506242899d874b18c09c0ed3d386870d0bd4273
Nitin Madnani <nmadnani@ets.org>
2022-08-02 11:44:06 -0400

    Remove sonokai.

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,4 @@
 local colorscheme = "tokyonight"
--- local colorscheme = "sonokai"
 
 -- Tokyonight colorscheme configuration
 vim.g.tokyonight_style = "night"
@@ -8,10 +7,6 @@ vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
 
 -- Change the "hint" color to the "orange" color, and make the "error" color bright red
 vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
-
--- sonokai colorscheme configuration
--- vim.g.sonokai_style = "atlantis"
--- vim.g.sonokai_enable_italic = 1
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then

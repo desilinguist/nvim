d39869b4492e693a4788321f15b8537907bfb37f
Nitin Madnani <nmadnani@ets.org>
2023-01-05 18:01:29 -0500

    Reduce number of packer jobs down to 10.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -31,7 +31,7 @@ end
 
 -- Have packer use a popup window
 packer.init({
-    max_jobs = 50,
+    max_jobs = 10,
 	display = {
 		open_fn = function()
 			return require("packer.util").float({ border = "rounded" })

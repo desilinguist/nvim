b6030c9ca4c4dcc57d71a0b8caa4e99429c08295
Nitin Madnani <nmadnani@ets.org>
2023-03-01 12:23:35 -0500

    Replace packer with lazy.

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,246 +1,209 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
-
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]])
-
--- Use a protected call so we don't error out on first use
-local status_ok, packer = pcall(require, "packer")
-if not status_ok then
-	return
-end
-
--- Have packer use a popup window
-packer.init({
-    max_jobs = 10,
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-        prompt_border = "rounded",
-	},
-})
+vim.opt.rtp:prepend(lazypath)
 
 -- Install your plugins here
-return packer.startup(function(use)
+local plugins = {
 	-- My plugins here
-	use("wbthomason/packer.nvim") -- Have packer manage itself
-	use("lewis6991/impatient.nvim") -- lua caching
-	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
-	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
-	use("windwp/nvim-autopairs") -- Autopairs, integrates with both cmp and treesitter
-	use("windwp/nvim-ts-autotag") -- Autocomplete and rename tags
-	use("numToStr/Comment.nvim") -- Easily comment stuff
-	use("kyazdani42/nvim-web-devicons") -- Use fancy icons for various file types
-	use("kyazdani42/nvim-tree.lua") -- Use a fancy filetree side bar
-	use("akinsho/bufferline.nvim") -- Easy graphical switching between open buffers
-	use("moll/vim-bbye") -- Easy buffer closing without messing up layouts
-	use("nvim-lualine/lualine.nvim") -- Fancy status line at the bottom
-	use("akinsho/toggleterm.nvim") -- Open terminals and terminal programs in floating windows
+    "lewis6991/impatient.nvim", -- cache everything
+    {
+        "folke/which-key.nvim",
+        event = 'BufReadPost',
+    },
+    "nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
+    "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
+    "windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
+    "windwp/nvim-ts-autotag", -- Autocomplete and rename tags
+    "numToStr/Comment.nvim", -- Easily comment stuff
+    "kyazdani42/nvim-web-devicons", -- Use fancy icons for various file types
+    "kyazdani42/nvim-tree.lua", -- Use a fancy filetree side bar
+    "akinsho/bufferline.nvim", -- Easy graphical switching between open buffers
+    "moll/vim-bbye", -- Easy buffer closing without messing up layouts
+    "nvim-lualine/lualine.nvim", -- Fancy status line at the bottom
+    "akinsho/toggleterm.nvim", -- Open terminals and terminal programs in floating windows
 
 	-- Colorschemes
-	use("folke/tokyonight.nvim")
+    "folke/tokyonight.nvim",
 
 	-- cmp plugins
-	use("hrsh7th/nvim-cmp") -- The completion plugin
-	use("hrsh7th/cmp-buffer") -- buffer completions
-	use("hrsh7th/cmp-path") -- path completions
-	use("hrsh7th/cmp-cmdline") -- cmdline completions
-	use("saadparwaiz1/cmp_luasnip") -- snippet completions
-	use("hrsh7th/cmp-nvim-lsp") -- lsp completions
-	use("hrsh7th/cmp-nvim-lua") -- nvim lua completions
-	use("hrsh7th/cmp-calc") -- calculator completions
-	use("lukas-reineke/cmp-under-comparator") -- better sort completion items that start with underlines
+    "hrsh7th/nvim-cmp", -- The completion plugin
+    "hrsh7th/cmp-buffer", -- buffer completions
+    "hrsh7th/cmp-path", -- path completions
+    "hrsh7th/cmp-cmdline", -- cmdline completions
+    "saadparwaiz1/cmp_luasnip", -- snippet completions
+    "hrsh7th/cmp-nvim-lsp", -- lsp completions
+    "hrsh7th/cmp-nvim-lua", -- nvim lua completions
+    "hrsh7th/cmp-calc", -- calculator completions
+    "lukas-reineke/cmp-under-comparator", -- better sort completion items that start with underlines
 
 	-- snippets
-	use("L3MON4D3/LuaSnip") --snippet engine
-	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
+    "L3MON4D3/LuaSnip", --snippet engine
+    "rafamadriz/friendly-snippets", -- a bunch of snippets to use
 
 	-- LSP
-	use("neovim/nvim-lspconfig") -- enable LSP
-	use("williamboman/mason.nvim") -- simple to use language server installer
-	use("williamboman/mason-lspconfig.nvim")
-	use("jose-elias-alvarez/null-ls.nvim") -- for formatters and linters
-	use("tamago324/nlsp-settings.nvim") -- Common null-ls settings
-	use("folke/trouble.nvim") -- Nice way to show LSP diagnostics
-	use("ray-x/lsp_signature.nvim") -- Show signature of any function while typing it
-	use("b0o/SchemaStore.nvim") -- Use SchemaStore for various JSON/YAML schemas
-	use("j-hui/fidget.nvim") -- Show LSP progress, if available
+    "neovim/nvim-lspconfig", -- enable LSP
+    "williamboman/mason.nvim", -- simple to use language server installer
+    "williamboman/mason-lspconfig.nvim",
+    "jose-elias-alvarez/null-ls.nvim", -- for formatters and linters
+    "tamago324/nlsp-settings.nvim", -- Common null-ls settings
+    "folke/trouble.nvim", -- Nice way to show LSP diagnostics
+    "ray-x/lsp_signature.nvim", -- Show signature of any function while typing it
+    "b0o/SchemaStore.nvim", -- Use SchemaStore for various JSON/YAML schemas
+    "j-hui/fidget.nvim", -- Show LSP progress, if available
 
 	-- Telescope
-	use("nvim-telescope/telescope.nvim")
-	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-	use({ "LinArcX/telescope-env.nvim" })
-	use({
-		"princejoogie/dir-telescope.nvim",
-		config = function()
-			require("dir-telescope").setup({
-				hidden = false,
-				respect_gitignore = true,
-			})
-		end,
-	})
+    "nvim-telescope/telescope.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    { "LinArcX/telescope-env.nvim" },
+    {
+        "princejoogie/dir-telescope.nvim",
+        config = function()
+            require("dir-telescope").setup({
+                hidden = false,
+                respect_gitignore = true,
+            })
+        end,
+    },
 
 	-- Treesitter
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-	})
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+},
 
 	-- Git
-	use("lewis6991/gitsigns.nvim")
+    "lewis6991/gitsigns.nvim",
 
 	-- Indentline
-	use("lukas-reineke/indent-blankline.nvim")
+    "lukas-reineke/indent-blankline.nvim",
 
 	-- alpha greeter
-	use("antoinemadec/FixCursorHold.nvim") -- This is needed to fix lsp doc highlight
-
-	-- whichkey
-	use({ "folke/which-key.nvim" })
+    "antoinemadec/FixCursorHold.nvim", -- This is needed to fix lsp doc highlight
 
 	-- neogen
-	use("danymat/neogen")
+    { "danymat/neogen", lazy=true },
 
 	-- colorizer
-	use("norcalli/nvim-colorizer.lua")
+    "norcalli/nvim-colorizer.lua",
 
 	-- filetype plugin replacing slow built-in filetype.vim
-	use("nathom/filetype.nvim")
+    "nathom/filetype.nvim",
 
 	-- sessions
-	use("olimorris/persisted.nvim")
+    { "olimorris/persisted.nvim", lazy=true },
 
 	-- stabilize
-	use("luukvbaal/stabilize.nvim")
+    "luukvbaal/stabilize.nvim",
 
 	-- cheatsheets
-	use("sudormrfbin/cheatsheet.nvim")
+    { "sudormrfbin/cheatsheet.nvim", lazy=true },
 
 	-- cursorline modes
-	use("mvllow/modes.nvim")
+    "mvllow/modes.nvim",
 
 	-- advanced text objects
-	use("wellle/targets.vim")
+    "wellle/targets.vim",
 
 	-- remove trailing whitespace and extra empty lines at the end
-	use({ "mcauley-penney/tidy.nvim" })
+    "mcauley-penney/tidy.nvim",
 
 	-- show colorcolumn but in virtual text
-	use("lukas-reineke/virt-column.nvim")
+    "lukas-reineke/virt-column.nvim",
 
 	-- plugin for easy browsing
-	use("lalitmee/browse.nvim")
+    { "lalitmee/browse.nvim", lazy=true },
 
 	-- plugin for surround operation
-	use("kylechui/nvim-surround")
+    "kylechui/nvim-surround",
 
 	-- plugin to show current scope in status line
-	use("SmiteshP/nvim-navic")
+    "SmiteshP/nvim-navic",
 
     -- undo-tree telescope plugin
-    use("debugloop/telescope-undo.nvim")
+    { "debugloop/telescope-undo.nvim", lazy=true },
 
 	-- plugin to make j/k movement faster
-	use({
-		"PHSix/faster.nvim",
-		event = { "VimEnter *" },
-		config = function()
-			-- vim.api.nvim_set_keymap("n", "j", "<Plug>(faster_move_j)", { noremap = false, silent = true })
-			-- vim.api.nvim_set_keymap("n", "k", "<Plug>(faster_move_k)", { noremap = false, silent = true })
-			-- or
-			vim.api.nvim_set_keymap("n", "j", "<Plug>(faster_move_gj)", { noremap = false, silent = true })
-			vim.api.nvim_set_keymap("n", "k", "<Plug>(faster_move_gk)", { noremap = false, silent = true })
-			-- if you need map in visual mode
-			-- vim.api.nvim_set_keymap('v', 'j', '<Plug>(faster_vmove_j)', {noremap=false, silent=true})
-			-- vim.api.nvim_set_keymap('v', 'k', '<Plug>(faster_vmove_k)', {noremap=false, silent=true})
-		end,
-	})
+    {
+        "PHSix/faster.nvim",
+        event = { "VimEnter *" },
+        config = function()
+            -- vim.api.nvim_set_keymap("n", "j", "<Plug>(faster_move_j)", { noremap = false, silent = true })
+            -- vim.api.nvim_set_keymap("n", "k", "<Plug>(faster_move_k)", { noremap = false, silent = true })
+            -- or
+            vim.api.nvim_set_keymap("n", "j", "<Plug>(faster_move_gj)", { noremap = false, silent = true })
+            vim.api.nvim_set_keymap("n", "k", "<Plug>(faster_move_gk)", { noremap = false, silent = true })
+            -- if you need map in visual mode
+            -- vim.api.nvim_set_keymap('v', 'j', '<Plug>(faster_vmove_j)', {noremap=false, silent=true})
+            -- vim.api.nvim_set_keymap('v', 'k', '<Plug>(faster_vmove_k)', {noremap=false, silent=true})
+        end,
+    },
 
 	-- treesitter text objects
-	use("nvim-treesitter/nvim-treesitter-textobjects")
+    "nvim-treesitter/nvim-treesitter-textobjects",
 
 	-- treesitter context
-	use("nvim-treesitter/nvim-treesitter-context")
+    "nvim-treesitter/nvim-treesitter-context",
 
 	-- nvim notify
-	use("rcarriga/nvim-notify")
+    "rcarriga/nvim-notify",
 
 	-- substitute
-	use("gbprod/substitute.nvim")
+    { "gbprod/substitute.nvim", lazy=true },
 
 	-- color picker
-	use({
-		"ziontee113/color-picker.nvim",
-		config = function()
-			require("color-picker")
-		end,
-	})
+    {
+        "ziontee113/color-picker.nvim",
+        config = function()
+            require("color-picker")
+        end,
+        lazy=true
+    },
 
 	-- icon picker
-	use("stevearc/dressing.nvim")
-	-- use({
-	-- 	"ziontee113/icon-picker.nvim",
-	-- 	config = function()
-	-- 		require("icon-picker").setup()
-	-- 	end,
-	-- })
+    "stevearc/dressing.nvim",
 
 	-- task runner
-	use("jedrzejboczar/toggletasks.nvim")
+    { "jedrzejboczar/toggletasks.nvim", lazy=true },
 
 	-- better sorting
-	use("SQVe/sort.nvim")
+    { "SQVe/sort.nvim", lazy=true },
 
 	-- better integration with system clipboard
-	use("ojroques/nvim-osc52")
+    "ojroques/nvim-osc52",
 
 	-- better folding
-	use({ "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" })
+    { "kevinhwang91/nvim-ufo", dependencies = { "kevinhwang91/promise-async" } },
 
 	-- better lsp diagnostics
-	use({ "ErichDonGubler/lsp_lines.nvim" })
+    { "ErichDonGubler/lsp_lines.nvim", lazy=true },
 
 	-- easy swapping of things
-	use({ "mizlan/iswap.nvim" })
+    { "mizlan/iswap.nvim", lazy=true },
 
 	-- better highlighting/filtering of todos etc. in comments
-	use("folke/todo-comments.nvim")
+    "folke/todo-comments.nvim",
 
 	-- smart inverter for wordsr
-	use({ "nguyenvukhang/nvim-toggler" })
+    { "nguyenvukhang/nvim-toggler", lazy=true },
 
 	-- autolist plugin for markdown
-	use({ "gaoDean/autolist.nvim" })
+    { "gaoDean/autolist.nvim", lazy=true },
 
 	-- silicon plugin for fancy code screenshots
-	use({ "narutoxy/silicon.lua" })
+    { "narutoxy/silicon.lua", lazy=true },
 
 	-- plugin to support nim
-	use("alaviss/nim.nvim")
+    { "alaviss/nim.nvim", ft="nim" },
+}
 
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
-end)
+local opts = {}
+require("lazy").setup(plugins, opts)

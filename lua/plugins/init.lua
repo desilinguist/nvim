e6c76621083d26fe09c942cf0b3ddc5193a6d400
Nitin Madnani <nmadnani@ets.org>
2023-04-16 14:10:25 -0400

    Add neoAI plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -195,5 +195,35 @@ return {
     -- plugin for generating gitignore files
     { "wintermute-cell/gitignore.nvim", lazy=true },
 
+    -- neoAI plugin
+    {
+        "Bryley/neoai.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+        },
+        cmd = {
+            "NeoAI",
+            "NeoAIOpen",
+            "NeoAIClose",
+            "NeoAIToggle",
+            "NeoAIContext",
+            "NeoAIContextOpen",
+            "NeoAIContextClose",
+            "NeoAIInject",
+            "NeoAIInjectCode",
+            "NeoAIInjectContext",
+            "NeoAIInjectContextCode",
+        },
+        keys = {
+            { "<leader>as", desc = "summarize text" },
+            { "<leader>ag", desc = "generate git message" },
+        },
+        config = function()
+            require("neoai").setup({
+                -- Options go here
+            })
+        end,
+    }
+
 
 }

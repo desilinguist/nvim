ebfad85f2ac6410c29bd4a377c5eb9fbf7178a52
Nitin Madnani <nmadnani@ets.org>
2024-03-04 17:58:31 -0500

    Fix lua ls config

--- lua/user/lsp/settings/lua_ls.lua
+++ lua/user/lsp/settings/lua_ls.lua
@@ -2,14 +2,19 @@ return {
 	settings = {
 
 		Lua = {
+			runtime = { version = "LuaJIT" },
 			diagnostics = {
 				globals = { "vim" },
 			},
 			workspace = {
+				checkThirdParty = false,
 				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
+					"#{3rd}/luv/library",
+					unpack(vim.api.nvim_get_runtime_file("", true)),
 				},
+			},
+			completion = {
+				callSnippet = "Replace",
 			},
 		},
 	},

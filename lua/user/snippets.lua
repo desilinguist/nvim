6a8cd50840ebb1b167a1150c5c40e4fb16db856c
Nitin Madnani <nmadnani@ets.org>
2024-03-12 13:26:39 -0400

    Add markdown snippets

--- lua/user/snippets.lua
+++ lua/user/snippets.lua
@@ -33,6 +33,8 @@ local function bash(_, _, command)
 	return res
 end
 
+require("luasnip.loaders.from_snipmate").load()
+
 ls.add_snippets(nil, {
 	all = {
 		snip({
@@ -86,18 +88,18 @@ ls.add_snippets(nil, {
 				"",
 				"    # set up an argument parser",
 				'    parser = argparse.ArgumentParser(prog="foo.py")',
-                '    parser.add_argument("--", dest="", help="")',
-                "",
-                "\t# parse given command line arguments",
-                "\targs = parser.parse_args()",
-                "",
-                "\t# set up the logging",
-                "\tlogging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)",
-                "",
-                "",
-                "if __name__ == '__main__':",
-                "\tmain()",
-                ""
+				'    parser.add_argument("--", dest="", help="")',
+				"",
+				"\t# parse given command line arguments",
+				"\targs = parser.parse_args()",
+				"",
+				"\t# set up the logging",
+				"\tlogging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)",
+				"",
+				"",
+				"if __name__ == '__main__':",
+				"\tmain()",
+				"",
 			}),
 			insert(0),
 		}),
@@ -157,72 +159,6 @@ ls.add_snippets(nil, {
 			text({ "local M = {}", "", "" }),
 			insert(0),
 			text({ "", "", "return M" }),
-		}),
-	},
-	markdown = {
-		-- Select link, press C-s, enter link to receive snippet
-		snip({
-			trig = "link",
-			namr = "markdown_link",
-			dscr = "Create markdown link [txt](url)",
-		}, {
-			text("["),
-			insert(1),
-			text("]("),
-			func(function(_, snip)
-				return snip.env.TM_SELECTED_TEXT[1] or {}
-			end, {}),
-			text(")"),
-			insert(0),
-		}),
-		snip({
-			trig = "codewrap",
-			namr = "markdown_code_wrap",
-			dscr = "Create markdown code block from existing text",
-		}, {
-			text("``` "),
-			insert(1, "Language"),
-			text({ "", "" }),
-			func(function(_, snip)
-				local tmp = {}
-				tmp = snip.env.TM_SELECTED_TEXT
-				tmp[0] = nil
-				return tmp or {}
-			end, {}),
-			text({ "", "```", "" }),
-			insert(0),
-		}),
-		snip({
-			trig = "codeempty",
-			namr = "markdown_code_empty",
-			dscr = "Create empty markdown code block",
-		}, {
-			text("``` "),
-			insert(1, "Language"),
-			text({ "", "" }),
-			insert(2, "Content"),
-			text({ "", "```", "" }),
-			insert(0),
-		}),
-		snip({
-			trig = "meta",
-			namr = "Metadata",
-			dscr = "Yaml metadata format for markdown",
-		}, {
-			text({ "---", "title: " }),
-			insert(1, "note_title"),
-			text({ "", "author: " }),
-			insert(2, "author"),
-			text({ "", "date: " }),
-			func(date, {}),
-			text({ "", "cathegories: [" }),
-			insert(3, ""),
-			text({ "]", "lastmod: " }),
-			func(date, {}),
-			text({ "", "tags: [" }),
-			insert(4),
-			text({ "]", "comments: true", "---", "" }),
-			insert(0),
 		}),
 	},
 	go = {

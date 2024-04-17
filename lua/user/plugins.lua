7f905e3908b71bf902a3cb3e54f213135f6474cd
Nitin Madnani <nmadnani@ets.org>
2024-04-17 09:03:24 -0400

    Set concurrency for lazy programmatically

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -13,8 +13,15 @@ vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
 
+-- set concurrency based on the number of available CPUs
+-- if that number is less than 4, set it to 1
+-- otherwise set it to 4
+local uv = require('luv')
+local cpus = uv.available_parallelism()
+local howconcurrent = cpus < 4 and 1 or 4
+
 -- add actual plugins to ~/.config/nvim/lua/plugins/init.lua
 local opts = {
-    concurrency = 4,
+    concurrency = howconcurrent,
 }
 require("lazy").setup("plugins", opts)

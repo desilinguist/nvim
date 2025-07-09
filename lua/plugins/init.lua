120f6dfd8650201f6af24eabbba78a7802023b54
Nitin Madnani <nmadnani@gmail.com>
2025-07-09 08:54:51 -0400

    Fix markview priority issue

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -442,12 +442,14 @@ return {
         -- markdown preview plugin
     {
         "OXY2DEV/markview.nvim",
-        lazy = false,
+        event = 'VeryLazy',
     },
     -- Treesitter
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
+        dependencies = { 'OXY2DEV/markview.nvim' },
+        lazy = false,
         config = function()
             require("nvim-treesitter.configs").setup({
                 ensure_installed = {
